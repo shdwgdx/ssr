833e7e503c8a1fcaf61a9bf2f588cfe36673a8da
unknown <shdwgdx@gmail.com>
2023-01-09 19:47:38 +0300

    first commit

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app> </v-navigation-drawer>
+
+    <v-app-bar app color="#000000"> </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app> </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+  data() {
+    return {
+      range: [-5, 5],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
